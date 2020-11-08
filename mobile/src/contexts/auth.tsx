4f98479419d344f6da2e42ccf214f22031717057
Suellen Lima Santos <suellen.santos@radixeng.com.br>
2020-11-08 19:30:04 -0300

    add loading

--- mobile/src/contexts/auth.tsx
+++ mobile/src/contexts/auth.tsx
@@ -1,7 +1,7 @@
 import React, {createContext, useState} from 'react';
 import { login } from '../services/api';
-import LoginResponse from './../interfaces/login-response.interface';
 import { User } from './../models/user';
+import { View, ActivityIndicator } from 'react-native';
 
 interface AuthContextData {
     signed: boolean,
@@ -14,14 +14,25 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({children}) => {
     const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(false);
     
     async function handleLogin(email: string, password: string) {
+        setLoading(true);
         var response = await login(email, password);
         setUser(response.user);
+        setLoading(false);
     }
 
     async function handleLogout() {
         setUser(null);
+    }
+
+    if (loading) {
+        return (
+            <View style={{alignItems: 'center', justifyContent: 'center', flex: 1,}}>
+                <ActivityIndicator size="large" color="#82B1B6" />
+            </View>
+        )
     }
 
     return (
